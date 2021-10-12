fdedceecbda3f168ee5e7906417840d0df4869fa
MikhasNew <87332000+MikhasNew@users.noreply.github.com>
2021-10-12 23:15:52 +0300

    Добавьте файлы проекта.

---
+++ Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameIndustry.Models
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
